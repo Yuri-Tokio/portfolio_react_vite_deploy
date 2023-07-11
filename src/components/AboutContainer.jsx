9c7bf941574af02b3cfbd5f5c728aaa8b379a697
Yuri Tokio <yuri@sheild.com.br>
2023-07-11 17:37:01 -0300

    Ajustes homePage Responsive and justify-text

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -6,7 +6,7 @@ const AboutContainer = () => {
       <div className="about">
         <h2>Apresentação</h2>
         <p>
-          Sou apaixonado por tecnologia e ser reconhecido por gerar soluções eficientes que impulsionem projects.
+          Sou apaixonado por tecnologia e ser reconhecido por gerar soluções eficientes que impulsionem projetos.
         </p>
         <p>
           Com um pouco mais de 2 anos de experiência em desenvolvimento web, ja trabalhei com ASP.NET, Node Js, SASS, React, Postman e GIT para criar soluções robustas e escaláveis que atendam às necessidades dos clientes.
