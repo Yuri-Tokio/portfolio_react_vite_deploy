7c687452bbcd0906ac83826d9ef15d64c22faa46
Yuri Tokio <yuri@sheild.com.br>
2023-07-12 03:42:43 -0300

    Grid responsive P1

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -17,12 +17,12 @@ const ProjectsPage = () => {
             <h1 className="title">Projetos</h1>
             <div className="projects-grid">
                 <div className="projects-card">
-                    <div className="img100">
+                    <div className="projects-info">
                         <img src={primeiroProjeto_resized} alt="Primeiro Projeto"></img>
                     </div>
                     <div className="projects-info">
                         <h2>Primeiro Projeto</h2>
-                        <p>Projeto Portfolio básico com Html, Css, JavaScript e algumas requisições via API</p>
+                        <p>Meu primeiro portfolio em HTML, CSS e JavaScript foi uma experiência emocionante, onde pude aplicar meus conhecimentos básicos para criar uma página web interativa e visualmente atraente</p>
                         <p><span>Tecnologias utilizadas:</span> Html, Css, JavaScript</p>
                         {pageIndex.map((portfolio) =>
                             <a href={portfolio.link}
@@ -31,18 +31,18 @@ const ProjectsPage = () => {
                     </div>
                 </div>
                 <div className="projects-card card-reverse">
-                    <div className="img100">
-                        <img src={MoviesLib_resized} alt="Movies Library"></img>
+                    <div className="projects-info">
+                        <img src={CV_resized} alt="Template Curriculum vitae"></img>
                     </div>
                     <div className="projects-info">
-                        <h2>Biblioteca de filmes</h2>
-                        <p>Uma biblioteca dos mais diversos filmes e animações desenvolvido para pesquisas e analises de dados com funcionalidades puramente observatórias e que abordam conceitos como Gerenciamento de API do TMDB, responsividade e desenvolvimento com diversos recursos do React</p>
-                        <p><span>Tecnologias utilizadas:</span> React, Vite, API (TMDB)</p>
-                        <a href="https://yuri-tokio.github.io/movies_lib/" target="_blank">Saber mais</a>
+                        <h2>Template Curriculum vitae</h2>
+                        <p>Template responsivo criado por mim de um projeto pessoal desenvolvido com tecnologias como React e SASS, além de conter meu currículo em pdf</p>
+                        <p><span>Tecnologias utilizadas:</span> React, Vite, SASS</p>
+                        <a href="https://yuri-tokio.github.io/CV/" target="_blank">Saber mais</a>
                     </div>
                 </div>
                 <div className="projects-card">
-                    <div className="img100">
+                    <div className="projects-info">
                         <img src={tasklist_resized} alt="tasklist"></img>
                     </div>
                     <div className="projects-info">
@@ -52,15 +52,15 @@ const ProjectsPage = () => {
                         <a href="https://github.com/Yuri-Tokio/react-tasklist" target="_blank">Saber mais</a>
                     </div>
                 </div>
-                <div className="projects-card card-reverse m-1">
-                    <div className="img100">
-                        <img src={CV_resized} alt="Template Curriculum vitae"></img>
+                <div className="projects-card card-reverse  m-1">
+                    <div className="projects-info">
+                        <img src={MoviesLib_resized} alt="Movies Library"></img>
                     </div>
                     <div className="projects-info">
-                        <h2>Template Curriculum vitae</h2>
-                        <p>Template responsivo criado por mim de um projeto pessoal desenvolvido com tecnologias como React e SASS, além de conter meu currículo em pdf</p>
-                        <p><span>Tecnologias utilizadas:</span> React, Vite, SASS</p>
-                        <a href="https://yuri-tokio.github.io/CV/" target="_blank">Saber mais</a>
+                        <h2>Biblioteca de filmes</h2>
+                        <p>Biblioteca de filmes e animações</p>
+                        <p><span>Tecnologias utilizadas:</span> React, Vite, API (TMDB)</p>
+                        <a href="https://yuri-tokio.github.io/movies_lib/" target="_blank">Saber mais</a>
                     </div>
                 </div>
             </div>
