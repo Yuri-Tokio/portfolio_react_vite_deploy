469874e71f997272f24a6bdb6577c06a646efeea
Yuri Tokio <yuri@sheild.com.br>
2023-08-07 02:27:56 -0300

    update textos

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -6,10 +6,6 @@ import MoviesLib_resized from "./../img/Projetos/MoviesLib_resized.png";
 import primeiroProjeto_resized from "./../img/Projetos/primeiroProjeto_resized.png";
 import tasklist_resized from "./../img/Projetos/tasklist_resized.png";
 
-const pageIndex = [
-    { name: "pageIndex", link: "https://yuri-tokio.github.io/Page-Index/" }
-]
-
 const ProjectsPage = () => {
     return (
         <section id="projects-page">
@@ -24,10 +20,10 @@ const ProjectsPage = () => {
                         <h2>Primeiro projeto</h2>
                         <p>Meu primeiro portfolio Web e Mobile em HTML, CSS e JavaScript foi uma experiência emocionante, onde pude aplicar meus conhecimentos básicos para criar uma página web interativa e atrativa</p>
                         <p><span>Tecnologias utilizadas:</span> Html, Css, JavaScript</p>
-                        {pageIndex.map((portfolio) =>
-                            <a href={portfolio.link}
-                                target="_blank" id={portfolio.name} key={portfolio.name}
-                            >Ver Projeto</a>)}
+                        <div className="linksProjetos">
+                            <a href="https://github.com/Yuri-Tokio/Page-Index" target="_blank">Ver Repositório</a>
+                            <a href="https://yuri-tokio.github.io/Page-Index/" target="_blank">Ver Projeto</a>
+                        </div>
                     </div>
                 </div>
                 <div className="projects-card card-reverse  m-1">
@@ -38,7 +34,10 @@ const ProjectsPage = () => {
                         <h2>Biblioteca de filmes</h2>
                         <p>Biblioteca de filmes e animações com informações detalhadas sobre cada título através da API do TMDB (The Movie Database).</p>
                         <p><span>Tecnologias utilizadas:</span> React, Vite, API (TMDB)</p>
-                        <a href="https://yuri-tokio.github.io/movies_lib/" target="_blank">Ver Projeto</a>
+                        <div className="linksProjetos">
+                            <a href="https://github.com/Yuri-Tokio/movies_lib" target="_blank">Ver Repositório</a>
+                            <a href="https://yuri-tokio.github.io/movies_lib/" target="_blank">Ver Projeto</a>
+                        </div>
                     </div>
                 </div>
                 <div className="projects-card">
@@ -49,7 +48,7 @@ const ProjectsPage = () => {
                         <h2>Lista de tarefas</h2>
                         <p>Lista de tarefas com abordagem em Node.Js e React. O projeto conta com uma api rodando em um servidor local para criação, alteração e exclusão das tarefas informadas</p>
                         <p><span>Tecnologias utilizadas:</span> React, Vite, API e Banco Local</p>
-                        <a href="https://github.com/Yuri-Tokio/react-tasklist" target="_blank">Ver Projeto</a>
+                            <a href="https://github.com/Yuri-Tokio/react-tasklist" target="_blank">Ver Repositório</a>
                     </div>
                 </div>
                 <div className="projects-card card-reverse">
@@ -60,7 +59,10 @@ const ProjectsPage = () => {
                         <h2>Template Curriculum vitae</h2>
                         <p>Template responsivo do meu currículo baseado no tema desse portfolio e criado com as mesmas tecnologias</p>
                         <p><span>Tecnologias utilizadas:</span> React, Vite, SASS</p>
-                        <a href="https://yuri-tokio.github.io/CV/" target="_blank">Ver Projeto</a>
+                        <div className="linksProjetos">
+                            <a href="https://github.com/Yuri-Tokio/CV" target="_blank">Ver Repositório</a>
+                            <a href="https://yuri-tokio.github.io/CV/" target="_blank">Ver Projeto</a>
+                        </div>
                     </div>
                 </div>
             </div>
