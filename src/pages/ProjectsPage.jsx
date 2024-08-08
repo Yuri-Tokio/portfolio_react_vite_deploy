b6ebc89e49acc97a8fb508cba613c3c5e72e2bda
Yuri Tokio <yuri@sheild.com.br>
2024-08-08 11:44:44 -0300

    Update area de projetos
    Retirei deploy pq nao deu certo com git actions

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,7 +1,7 @@
 import ArrowBack from "../components/ArrowBack";
 import "../styles/components/pages/projectspage.sass";
 
-import CV_resized from "./../img/Projetos/CV_resized.png";
+import Base from "./../img/Projetos/base.png";
 import MoviesLib_resized from "./../img/Projetos/MoviesLib_resized.png";
 import primeiroProjeto_resized from "./../img/Projetos/primeiroProjeto_resized.png";
 import tasklist_resized from "./../img/Projetos/tasklist_resized.png";
@@ -12,27 +12,14 @@ const ProjectsPage = () => {
             <ArrowBack />
             <h1 className="title">Projetos</h1>
             <div className="projects-grid">
+
                 <div className="projects-card">
-                    <div className="projects-info">
-                        <img src={primeiroProjeto_resized} alt="Primeiro Projeto"></img>
-                    </div>
-                    <div className="projects-info">
-                        <h2>Primeiro projeto</h2>
-                        <p>Meu primeiro portfolio Web e Mobile em HTML, CSS e JavaScript foi uma experiência emocionante, onde pude aplicar meus conhecimentos básicos para criar uma página web interativa e atrativa</p>
-                        <p><span>Tecnologias utilizadas:</span> Html, Css, JavaScript</p>
-                        <div className="linksProjetos">
-                            <a href="https://github.com/Yuri-Tokio/Page-Index" target="_blank">Ver Repositório</a>
-                            <a href="https://yuri-tokio.github.io/Page-Index/" target="_blank">Ver Projeto</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="projects-card card-reverse  m-1">
                     <div className="projects-info">
                         <img src={MoviesLib_resized} alt="Movies Library"></img>
                     </div>
                     <div className="projects-info">
                         <h2>Biblioteca de filmes</h2>
-                        <p>Biblioteca de filmes e animações com informações detalhadas sobre cada título através da API do TMDB (The Movie Database).</p>
+                        <p>Desenvolvi uma aplicação em React usando Vite e a API do TMDB, que permite explorar filmes e animações com informações detalhadas sobre cada título. Utilizei React Router para navegação e React Hooks para gerenciamento de estado, criando uma experiência de usuário fluida</p>
                         <p><span>Tecnologias utilizadas:</span> React, Vite, API (TMDB)</p>
                         <div className="linksProjetos">
                             <a href="https://github.com/Yuri-Tokio/movies_lib" target="_blank">Ver Repositório</a>
@@ -40,7 +27,8 @@ const ProjectsPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="projects-card">
+
+                <div className="projects-card card-reverse">
                     <div className="projects-info">
                         <img src={tasklist_resized} alt="tasklist"></img>
                     </div>
@@ -48,23 +36,61 @@ const ProjectsPage = () => {
                         <h2>Lista de tarefas</h2>
                         <p>Lista de tarefas com abordagem em Node.Js e React. O projeto conta com uma api rodando em um servidor local para criação, alteração e exclusão das tarefas informadas</p>
                         <p><span>Tecnologias utilizadas:</span> React, Vite, API e Banco Local</p>
-                            <a href="https://github.com/Yuri-Tokio/react-tasklist" target="_blank">Ver Repositório</a>
+                        <a href="https://github.com/Yuri-Tokio/react-tasklist" target="_blank">Ver Repositório</a>
                     </div>
                 </div>
-                <div className="projects-card card-reverse">
+
+            </div>
+
+            <h1 className="title">Projetos que estão por vir...</h1>
+
+            <div className="projects-grid">
+
+                <div className="projects-card">
                     <div className="projects-info">
-                        <img src={CV_resized} alt="Template Curriculum vitae"></img>
+                        <img src={Base} alt="Movies Library"></img>
                     </div>
                     <div className="projects-info">
-                        <h2>Template Curriculum vitae</h2>
-                        <p>Template responsivo do meu currículo baseado no tema desse portfolio e criado com as mesmas tecnologias</p>
-                        <p><span>Tecnologias utilizadas:</span> React, Vite, SASS</p>
+                        <h2>Area de login autenticado com JWT </h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi tempora, exercitationem vitae nesciunt iure corporis ipsum ipsam animi quam accusamus perspiciatis neque quisquam iste veniam voluptatibus cum totam id quia atque facilis ut obcaecati error. Aperiam numquam perferendis quasi porro veritatis, cumque, aliquam maiores magnam rerum est harum nam itaque.</p>
+                        <p><span>Tecnologias utilizadas:</span></p>
                         <div className="linksProjetos">
-                            <a href="https://github.com/Yuri-Tokio/CV" target="_blank">Ver Repositório</a>
-                            <a href="https://yuri-tokio.github.io/CV/" target="_blank">Ver Projeto</a>
+                            <a href="https://github.com/Yuri-Tokio/movies_lib" target="_blank">Ver Repositório</a>
+                            <a href="https://yuri-tokio.github.io/movies_lib/" target="_blank">Ver Projeto</a>
                         </div>
                     </div>
                 </div>
+
+                <div className="projects-card  card-reverse">
+                    <div className="projects-info">
+                        <img src={Base} alt="tasklist"></img>
+                    </div>
+                    <div className="projects-info">
+                        <h2>Jogo da velha com WebSockets</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non reprehenderit labore molestiae blanditiis quam laborum ut ducimus! Magnam deserunt est id explicabo pariatur exercitationem laborum ad porro nemo? Unde illo hic aliquam molestiae ea quia ab, cupiditate praesentium sunt deserunt minus asperiores facere neque alias, necessitatibus error vitae et eius?</p>
+                        <p><span>Tecnologias utilizadas:</span></p>
+                        <div className="linksProjetos">
+                            <a href="#">Ver Repositório</a>
+                            <a href="#">Ver Projeto</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="projects-card">
+                    <div className="projects-info">
+                        <img src={Base} alt="Movies Library"></img>
+                    </div>
+                    <div className="projects-info">
+                        <h2>Catalogo de Produtos otimizado com cloudflare</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi tempora, exercitationem vitae nesciunt iure corporis ipsum ipsam animi quam accusamus perspiciatis neque quisquam iste veniam voluptatibus cum totam id quia atque facilis ut obcaecati error. Aperiam numquam perferendis quasi porro veritatis, cumque, aliquam maiores magnam rerum est harum nam itaque.</p>
+                        <p><span>Tecnologias utilizadas:</span></p>
+                        <div className="linksProjetos">
+                            <a href="#">Ver Repositório</a>
+                            <a href="#">Ver Projeto</a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </section>
     )
